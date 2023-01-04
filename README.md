049633b7d3f7d0fbf3b7774b156ad486909d7bc3
Ogundepo Odunayo <ogundepoodunayo@gmail.com>
2023-01-04 13:35:10 -0500

    add text classification workflow

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ print(list_datasets(task='text_classification'))
 | swahili_news                  | [website](https://doi.org/10.5281/zenodo.5514203)       | -   | Yes | `train` `dev` | [link](https://huggingface.co/datasets/swahili_news) | `sw` |
 | hausa_voa_topics              | -   | -   | Yes | `train` `dev` `test` | [link](https://huggingface.co/datasets/hausa_voa_topics) | `ha` |
 | yoruba_bbc_topics             | -   | -   | Yes | `train` `dev` `test` | [link](https://huggingface.co/datasets/yoruba_bbc_topics) | `yo` |
-| Davis/Swahili-tweet-sentiment | [website](https://github.com/Davisy/Swahili-Tweet-Sentiment-Analysis-App)       | -   | Yes | | [link](https://huggingface.co/datasets/Davis/Swahili-tweet-sentiment) | `sw` |
-| Iyanuoluwa/YOSM               | [website](https://github.com/IyanuSh/YOSM)       | [paper](https://arxiv.org/abs/2204.09711)   | Yes | `train` `dev` `test` | [link](https://huggingface.co/datasets/Iyanuoluwa/YOSM) | `yo` |
+| swahili-tweet-sentiment | [website](https://github.com/Davisy/Swahili-Tweet-Sentiment-Analysis-App)       | -   | Yes | | [link](https://huggingface.co/datasets/Davis/Swahili-tweet-sentiment) | `sw` |
+| YOSM               | [website](https://github.com/IyanuSh/YOSM)       | [paper](https://arxiv.org/abs/2204.09711)   | Yes | `train` `dev` `test` | [link](https://huggingface.co/datasets/Iyanuoluwa/YOSM) | `yo` |
 
 
 
--- bean/evaluate/evaluate_classifier.py
+++ bean/evaluate/evaluate_classifier.py
@@ -1,6 +1,11 @@
 import torch
 import logging
 import evaluate
+from tqdm import tqdm
+from datasets import Dataset
+from typing import List
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_recall_fscore_support
 from bean.evaluate import BaseEvaluate
 from accelerate import Accelerator
 from torch.utils.data import DataLoader
@@ -29,15 +34,29 @@ class TextClassificationEvaluate(BaseEvaluate):
         self.__dict__.update(kwargs)
 
         self.accelerator = self._initialize_accelerator()
+        self.metrics = self._load_metrics()
 
-    def evaluate(self):
-        pass
+    def evaluate(self, dataset: Dataset):
+        """
+
+        """
+        tokenized_dataset = self.process_dataset_split(dataset)
+        dataloader = self._create_dataloader(tokenized_dataset)
+        predictions, references = self.inference(dataloader)
+
+        precision, recall, f1, _ = precision_recall_fscore_support(references, predictions, average="macro")
+        acc = accuracy_score(references, predictions)
+        return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
+
 
     def inference(self, data_loader: DataLoader):
+        """
+        Generate prediction and ground-truth target pair from eval dataloader
+        """
         self.model.eval()
         samples_seen = 0
 
-        for step, batch in enumerate(data_loader):
+        for step, batch in tqdm(enumerate(data_loader), total=len(data_loader)):
             with torch.no_grad():
                 outputs = self.model(**batch)
         predictions = outputs.logits.argmax(dim=-1)
@@ -51,11 +70,20 @@ class TextClassificationEvaluate(BaseEvaluate):
 
         return predictions, references
 
-    def load_metrics(self):
-        metrics = [evaluate.load(metric) for metric in text_classification_metrics]
+    def _load_metrics(self) -> List:
+        """
+        load metrics for evaluation
+        """
+        metrics = [evaluate.load(metric, average='weighted') for metric in text_classification_metrics]
         return metrics
 
-    def _create_dataloader(self, dataset_collection):
+    def _create_dataloader(self, tokenized_dataset: Dataset) -> DataLoader:
+        """
+        Create a dataloader from the tokenized dataset
+        tokenized_dataset: Dataset ==> tokenized dataset
+        returns:
+        dataloader: DataLoader ==> DataLoader
+        """
         # DataLoaders creation:
         if self.pad_to_max_length:
             # If padding was already done ot max length, we use the default data collator that will just convert everything
@@ -67,38 +95,50 @@ class TextClassificationEvaluate(BaseEvaluate):
             # of 8s, which will enable the use of Tensor Cores on NVIDIA hardware with compute capability >= 7.5 (Volta).
             data_collator = DataCollatorWithPadding(self.tokenizer, pad_to_multiple_of=(8 if self.accelerator.use_fp16 else None))
         dataloader = DataLoader(
-            dataset_collection, shuffle=True, collate_fn=data_collator, batch_size=self.batch_size
+            tokenized_dataset, shuffle=True, collate_fn=data_collator, batch_size=self.batch_size
         )
         return dataloader
 
-    def process_dataset_split(self, dataset_collection, split):
+    def process_dataset_split(self, dataset_collection: Dataset) -> Dataset:
+        """
+        This function uses the _tokenize_dataset method to tokenize and map dataset labels
+        dataset_collection: Dataset ==> raw dataset
+        returns:
+        processed_datasets: Dataset ==> tokenized Dataset
+        """
         with self.accelerator.main_process_first():
             processed_datasets = dataset_collection.map(
                 self._tokenize_dataset,
                 batched=True,
-                remove_columns=dataset_collection[split].column_names,
+                remove_columns=dataset_collection.column_names,
                 desc="Running tokenizer on dataset",
             )
 
         return processed_datasets
 
-    def _tokenize_dataset(self, examples):
+    def _tokenize_dataset(self, examples: Dataset) -> Dataset:
+        """
+        This function tokenizes the raw datasets and maps the target feature to ids
+        examples: Datasets ==> raw text dataset
+        returns:
+        result: Datasets ==>  tokenized dataset
+        """
         # Tokenize the texts
-        texts = ((examples[self.text_column_name],))
+        texts = ((examples[self.text_feature_name],))
         result = self.tokenizer(*texts, padding=self.padding, max_length=self.max_length, truncation=self.truncation)
 
         if "label" in examples:
-            if self.label_to_id is not None:
+            if not self.label_mapped:
                 result["labels"] = [self.label_to_id[l] for l in examples["label"]]
             else:
                 # In all cases, rename the column to labels because the model will expect that.
-                result["labels"] = examples["label"]
+                result["labels"] = examples[self.label_feature_name]
         return result
 
-    def _initialize_accelerator(self):
-        accelerator = (
-            Accelerator(log_with=self.report_to, logging_dir=self.output_dir) if self.with_tracking else Accelerator()
-        )
-
+    def _initialize_accelerator(self) -> Accelerator:
+        """
+        Initialize accelerator; used for managing device placement for easy distribution
+        """
+        accelerator = Accelerator()
         return accelerator
     
