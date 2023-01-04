a126a7a2c57fa882c58a52aebae3341ceddc3ffe
ToluClassics <ogundepoodunayo@gmail.com>
2023-01-03 19:07:54 -0500

    updates

--- bean/evaluate/base_evaluate.py
+++ bean/evaluate/base_evaluate.py
@@ -1,12 +1,17 @@
+from abc import abstractmethod
+
 class BaseEvaluate:
     def __init__(self):
         pass
 
+    @abstractmethod
     def preprocess_dataset(self):
         pass
 
+    @abstractmethod
     def load_metrics(self):
         pass
 
+    @abstractmethod
     def evaluate(self):
         pass
---
+++ bean/evaluate/evaluate_classifier.py
@@ -0,0 +1,6 @@
+from bean.evaluate import BaseEvaluate
+
+class TextClassificationEvaluate(BaseEvaluate):
+    def __init__(self, model_path_or_name: str, ):
+        super().__init__()
+    
