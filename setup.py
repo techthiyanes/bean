ee7fd6709797140e1f2d43a735a385c8e7eafee3
Iyanuoluwa Shode <66849230+IyanuSh@users.noreply.github.com>
2023-01-05 18:33:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('requirements.txt') as f:
 setup(
     name="bean",
     version="0.0.1",
-    author="Ogundepo Odunayo",
-    author_email="oogundep@uwaterloo.ca",
+    author="Ogundepo Odunayo", "Iyanuoluwa Shode",
+    author_email="oogundep@uwaterloo.ca", "shodei1@montclair.edu",
     description="BEnchmarking Africa NLP (BeAN 🫘), A framework for easy evaluation of language models on several Africa NLP datasets",
     packages=find_packages(),
     url="",
